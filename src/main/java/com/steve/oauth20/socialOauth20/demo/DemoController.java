3bc63dc061af357977fdd845a6f024cfc10ee38f
Stephen Owino <36981339s@gmail.com>
2023-09-07 13:12:49 +0300

    Initial commit

---
+++ src/main/java/com/steve/oauth20/socialOauth20/demo/DemoController.java
@@ -0,0 +1,18 @@
+package com.steve.oauth20.socialOauth20.demo;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/demo")
+public class DemoController {
+
+    @GetMapping
+    public ResponseEntity<String> hello(){
+       return ResponseEntity.ok("Hello from StephenotienoOAuth2.o");
+    }
+
+}
---
+++ src/main/java/com/steve/oauth20/socialOauth20/security/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.steve.oauth20.socialOauth20.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(withDefaults());
+        return http.build();
+
+
+    }
+    }
+
+    //Client ID: Iv1.3b3ba18b22ff8ea9
+
+//Client secret
+// f5270c3d5c47eac1c00009d9092baf1cb34b5869
+
